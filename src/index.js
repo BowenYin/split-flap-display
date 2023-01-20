b47de7b40860d5ab654693913da6f8dfac1c40f3
Bowen Yin <bowenvictoryin@gmail.com>
2023-01-20 06:47:12 +0000

    Fix

--- src/index.js
+++ src/index.js
@@ -21,49 +21,6 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app);
 
-let text = "Hello, World the quick, brown fox jumps over the lazy dog and barks at it";
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
 class Display extends React.Component {
   constructor(props) {
     super(props);
@@ -134,13 +91,6 @@ class Display extends React.Component {
           timing={100}
           hinge={false}
         />
-        {/* <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{}</div>
-          <ol>{}</ol>
-        </div> */}
       </div>
     );
   }
